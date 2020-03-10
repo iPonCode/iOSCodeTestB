ce5b3d3ef053748e29f59625e2b6aadf1a348552
Simon <simon@aparicio.org>
2020-03-10 23:12:51 +0100

    Initial commit: Create project and the basic folder structure

---
+++ iOSCodeTestB/iOSCodeTestB/Source/Transactions/View/ViewController.swift
@@ -0,0 +1,19 @@
+//  ViewController.swift
+//  iOSCodeTestB
+//
+//  Created by Simón Aparicio on 10/03/2020.
+//  Copyright © 2020 iPon.es. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
